609b52e905f3bd4fe13d43c5ba242f537efb77bf
caarlosdamian <dapecarlos@gmail.com>
2021-08-18 20:31:07 -0500

    complete intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
-import '../../styles/intro.scss'
-
+import "../../styles/intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-        
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: false,
+      backDelay:  1500,
+      backSpeed:60,
+      showCursor:true,
+      strings: ["React Guru","Freelancer","Cat Lover", "Content Creator"],
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man2.png" alt="myPhoto" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Carlos Damian</h1>
+          <h3>Developer <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portafolio">
+          <img src="assets/down.png" alt="down-arrow" />
+        </a>
+      </div>
+    </div>
+  );
 }
