7803110d49f8a5420cccf1645b4aa118c9fdefd9
caarlosdamian <dapecarlos@gmail.com>
2021-08-19 21:35:57 -0500

    add emailjs-com

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import "../../styles/contact.scss";
+import emailjs from "emailjs-com";
 
 export default function Contact({ setMenuOpen, nightMode }) {
-  const [message, setMessage] = useState(false)
-  const handleSubmit =(e)=>{
-    e.preventDefault()
-    setMessage(true)
-  }
+  const [message, setMessage] = useState(false);
+  console.log(process.env.REACT_APP_EMAIL_API_KEY);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_nvllena",
+        "template_zzxueu9",
+        e.target,
+        `${process.env.REACT_APP_EMAIL_API_KEY}`
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    setMessage(true);
+    e.target.reset();
+  };
   return (
     <div
       className={"contact " + (nightMode && "night")}
@@ -19,14 +37,20 @@ export default function Contact({ setMenuOpen, nightMode }) {
       <div className="right">
         <h2>Contact</h2>
         <form action="" onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
-          <button type="submit">Send</button>
+          <input type="email" placeholder="Email" name="email" />
+          <textarea
+            name="message"
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button type="submit" value="Send">
+            Send
+          </button>
           {message && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
       </div>
-
-
     </div>
   );
 }
